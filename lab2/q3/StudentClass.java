9415607eda652a0b2a0da095436da499432c2ea8
Jasleen <jasleen.j431@gmail.com>
2021-03-04 01:01:18 +0530

    My First commit

---
+++ lab2/q3/StudentClass.java
@@ -0,0 +1,18 @@
+class StudentClass
+{
+int StudentAge;	//state
+
+public void setAge(int age){
+StudentAge = age;
+}
+public int getAge(){
+System.out.println("Student's age is : "+ StudentAge);
+return StudentAge;
+}
+public static void main(String args[]){
+//object creation
+StudentClass S1 = new StudentClass();
+S1.setAge(18);
+S1.getAge();
+}
+}
